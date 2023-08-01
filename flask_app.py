e3906556d0ab8608cc253d8f57e72dbd3f127119
Akashrajasekar <47441702+Akashrajasekar@users.noreply.github.com>
2023-08-01 23:22:27 +0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -17,6 +17,7 @@ db_config = {
 }
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/CarbonEstimate/credentials.json'
 
+
 def extract_text_from_image(image):
     client = vision_v1.ImageAnnotatorClient()
 
@@ -49,36 +50,27 @@ def extract_text_from_pdf(pdf_file):
 
 @app.route('/')
 def home():
-    return render_template('web.html')
+    return render_template('homepage.html')
 
-'''@app.route('/carbondubai', methods=['GET'])
-def extract_text_dubai():
-    if 'file1' not in request.files:
-        return jsonify({'error': 'No file found.'}), 400
+@app.route('/electricity.html')
+def electricity():
+    return render_template('electricity.html')
 
-    file = request.files['file1']
-    if file.filename == '':
-        return jsonify({'error': 'Invalid file.'}), 400
+@app.route('/vehicle.html')
+def vehiclepage():
+    return render_template('vehicle.html')
 
-    if file.filename.lower().endswith('.pdf'):
-        extracted_texts = extract_text_from_pdf(file)
-        # Add any further processing if needed
-        carbon = extracted_texts[1]['lines'].index("Kg CO2e")
-        return jsonify({"carbon emission" : str(extracted_texts[1]['lines'][carbon+1]) + " kg CO2e"})
+@app.route('/flight.html')
+def flightpage():
+    return render_template('flight.html')
 
-    elif file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-        extracted_texts = extract_text_from_image(Image.open(file))
-        # Add any further processing if needed
-        carbon = extracted_texts.index("Kg CO2e")
-        return jsonify({"carbon emission" : str(extracted_texts[carbon+1]) + " kg CO2e"})
-
-
-    else:
-        return jsonify({'error': 'Unsupported file format.'}), 400'''
+@app.route('/results.html')
+def results():
+    return render_template('results.html')
 
 
 
-@app.route('/carbondubai', methods=['POST'])
+@app.route('/carbondubai', methods=['GET'])
 def extract_text_dubai():
     if 'file1' not in request.files:
         return jsonify({'error': 'No file found.'}), 400
@@ -102,8 +94,6 @@ def extract_text_dubai():
 
     else:
         return jsonify({'error': 'Unsupported file format.'}), 400
-
-    #return "File uploaded sucessfully"
 
 
 @app.route('/carbonabud', methods=['GET'])
@@ -136,21 +126,9 @@ def extract_text_abu():
         return jsonify({'error': 'Unsupported file format.'}), 400
 
 
+
 @app.route('/carbonfewa', methods=['GET'])
 def extract_text_fewa():
-    '''if 'pdf' not in request.files:
-        return jsonify({'error': 'No PDF file found.'}), 400
-
-    pdf_file = request.files['pdf2']
-
-    if pdf_file.filename == '':
-        return jsonify({'error': 'Invalid PDF file.'}), 400
-
-    extracted_texts = extract_text_from_pdf(pdf_file)
-    #return jsonify({"pages" : extracted_texts})
-    carbon = extracted_texts[0]['lines'].index("upto 800")
-    return jsonify({"carbon emission" : str(extracted_texts[0]['lines'][carbon-1]) + " kg CO2e"})'''
-
     if 'file01' not in request.files:
         return jsonify({'error': 'No file found.'}), 400
 
@@ -178,31 +156,6 @@ def extract_text_fewa():
 
 @app.route('/carbonshrj', methods=['GET'])
 def extract_text_shrj():
-    '''if 'pdf' not in request.files:
-        return jsonify({'error': 'No PDF file found.'}), 400'''
-
-    '''if 'pdf' in request.files:
-        pdf_file = request.files['pdf3']
-        if pdf_file.filename == '':
-            return jsonify({'error': 'Invalid PDF file.'}), 400
-        extracted_texts = extract_text_from_pdf(pdf_file)
-        #return jsonify({"pages" : extracted_texts})
-        kwh = extracted_texts[0]['lines'].index("Consumption")
-        carbon = float(extracted_texts[0]['lines'][kwh+1]) * 0.40
-        return jsonify({"carbon emission" : str(carbon) + " kg CO2e"})
-
-
-    elif 'jpg' in request.files:
-        jpg_file = request.files['pdf3']
-        if jpg_file.filename == '':
-            return jsonify({'error': 'Invalid JPG file.'}), 400
-        extracted_texts = extract_text_from_image(Image.open(jpg_file))
-        # Add any further processing if needed
-        return jsonify({"pages": extracted_texts})
-
-    else:
-        return jsonify({'error': 'No PDF or JPG file found.'}), 400'''
-
     if 'file' not in request.files:
         return jsonify({'error': 'No file found.'}), 400
 
@@ -358,6 +311,10 @@ def trans_emission(fuel, veh_type, distance):
 
     return emission
 
+@app.route('/offset', methods=['GET'])
+def offset():
+    carbonoffset=220.2*(float(request.args.get('total_emission')))
+    return jsonify({"offset" : str(carbonoffset)})
 
 @app.route('/carbontotal', methods=['GET'])
 def carbon_total():
@@ -428,102 +385,242 @@ def carbon_total():
     total.append(fly_tot)
 
     carbon_emission=sum(total)/1000
-    return jsonify({"total carbon emission": str(carbon_emission) + " t CO2e"})
+    price=calculate_money_for_carbon_offset(carbon_emission)
+    return jsonify({"total carbon emission": str(carbon_emission) + " t CO2e",
+                    "Offset price": "AED. " +str(price) })
+
+
+'''@app.route('/electricity', methods=['POST'])
+def upload():
+    total=[]
+    emirate = request.form.get('emirate')
+
+    if not emirate:
+        return {'error': 'Emirate value not provided.'}
+
+    emirate = emirate.lower()
+
+    if emirate not in ['dubai', 'abu dhabi', 'sharjah', 'ajman', 'fujairah', 'umm al quwain', 'ras al khaimah']:
+        return {'error': 'Invalid emirate. Supported emirates are: dubai, abudhabi, sharjah, fewa'}
+
+
+    inp_file = request.files.get('file')
+
+    if inp_file is None or inp_file.filename == '':
+        return {'error': 'Invalid file.'}
+
+    if inp_file.filename.lower().endswith('.pdf'):
+            extracted_texts = extract_text_from_pdf(inp_file)
+            # Add any further processing if needed
+
+            if emirate == 'abu dhabi':
+                kwh = extracted_texts[2]['lines'].index("Total units")
+                carbon = float(extracted_texts[2]['lines'][kwh+1]) * 0.39
+            elif emirate == 'dubai':
+                carbon = extracted_texts[1]['lines'].index("Kg CO2e")
+                carbon = float(extracted_texts[1]['lines'][carbon+1])
+            elif emirate == 'sharjah':
+                kwh = extracted_texts[0]['lines'].index("Consumption")
+                carbon = float(extracted_texts[0]['lines'][kwh+1]) * 0.40
+            elif emirate == 'ajman' or emirate == 'fujairah' or emirate == 'umm al quwain' or emirate == 'ras al khaimah' :
+                carbon = extracted_texts[0]['lines'].index("upto 800")
+                carbon = float(extracted_texts[0]['lines'][carbon-1])
+
+            total.append(carbon)
+
+    elif inp_file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+            extracted_texts = extract_text_from_image(Image.open(inp_file))
+            # Add any further processing if needed
+
+            if emirate == 'abu dhabi':
+                kwh1 = extracted_texts.index("Total units")
+                carbon1 = float(extracted_texts[kwh1+1]) * 0.39
+            elif emirate == 'dubai':
+                carbon = extracted_texts.index("Kg CO2e")
+                carbon1 = float(extracted_texts[carbon+1])
+            elif emirate == 'sharjah':
+                kwh = extracted_texts.index("Previous Reading Date Consumption")
+                carbon1 = float(extracted_texts[kwh+2]) * 0.40
+            elif emirate == 'ajman' or emirate == 'fujairah' or emirate == 'umm al quwain' or emirate == 'ras al khaimah' :
+                carbon = extracted_texts.index("upto 800")
+                carbon1 = float(extracted_texts[carbon-1])
+
+            total.append(carbon1)
+
+    else:
+        return {'error': 'Unsupported file format.'}
+
+    num_veh = int(request.form.get("vehicle_no", 1))
+
+    if num_veh!=0:
+        veh = []
+        for i in range(1, num_veh + 1):
+            veh_type = request.form.get(f"Vehicle{i}")
+            fuel = request.form.get(f"Fuel Type{i}")
+            distance = float(request.form.get(f"Distance{i}"))
+
+            emission = trans_emission(fuel, veh_type, distance)
+            veh.append(emission)
+
+        veh_tot = sum(veh)
+    else:
+        veh_tot=0
+
+    total.append(veh_tot)
+
+    url = "https://www.carboninterface.com/api/v1/estimates"
+    headers = {
+        "Authorization": "Bearer Eq8vmxlGJFDQJWlxXPb5A",
+        "Content-Type": "application/json"
+    }
+    passengers = request.form["passengers"]
+    num_legs = int(request.form.get("Trips", 1))  # Number of trips, defaulting to 1 if not provided
+
+    if (num_legs != 0 and passengers != 0) or num_legs != 0:
+        legs = []
+        for i in range(1, num_legs + 1):
+            departure_airport = request.form[f"From{i}"]
+            destination_airport = request.form[f"To{i}"]
+            legs.append({
+                "departure_airport": departure_airport,
+                "destination_airport": destination_airport
+            })
+
+        payload = {
+            "type": "flight",
+            "passengers": passengers,
+            "legs": legs,
+            "distance_unit": "km",
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        carbon_trans = response.json()["data"]["attributes"]["carbon_kg"]
+    else:
+        carbon_trans = 0
+
+    total.append(float(carbon_trans))
+
+
+
+    carbon_emission=sum(total)/1000
+    return jsonify({"total_carbon_emission": str(carbon_emission) + " t CO2e"})'''
 
 
 @app.route('/electricity', methods=['POST'])
 def upload():
-    if 'file' not in request.files:
-        return "No file part"
-
-    file = request.files.get('file')
     emirate = request.form.get('emirate')
 
-    if file.filename == '':
-        return "No selected file"
+    if not emirate:
+        return {'error': 'Emirate value not provided.'}
 
-    # Handle the file based on the selected emirate
-    if emirate == 'Dubai':
-            if file.filename.lower().endswith('.pdf'):
-                extracted_texts = extract_text_from_pdf(file)
-                # Add any further processing if needed
-                carbon = extracted_texts[1]['lines'].index("Kg CO2e")
-                return jsonify({"carbon emission" : str(extracted_texts[1]['lines'][carbon+1]) + " kg CO2e"})
+    emirate = emirate.lower()
 
-            elif file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-                extracted_texts = extract_text_from_image(Image.open(file))
-                # Add any further processing if needed
-                carbon = extracted_texts.index("Kg CO2e")
-                return jsonify({"carbon emission" : str(extracted_texts[carbon+1]) + " kg CO2e"})
+    if emirate not in ['dubai', 'abu dhabi', 'sharjah', 'ajman', 'fujairah', 'umm al quwain', 'ras al khaimah']:
+        return {'error': 'Invalid emirate. Supported emirates are: dubai, abudhabi, sharjah, fewa'}
 
+    inp_file = request.files.get('file')
 
-            else:
-                return jsonify({'error': 'Unsupported file format.'}), 400
-                pass
+    if inp_file is None or inp_file.filename == '':
+        return {'error': 'Invalid file.'}
 
-    elif emirate == 'Abu Dhabi':
-        if file.filename.lower().endswith('.pdf'):
-            extracted_texts = extract_text_from_pdf(file)
-            # Add any further processing if needed
+    if inp_file.filename.lower().endswith('.pdf'):
+        extracted_texts = extract_text_from_pdf(inp_file)
+        # Add any further processing if needed
+
+        if emirate == 'abu dhabi':
             kwh = extracted_texts[2]['lines'].index("Total units")
-            carbon = float(extracted_texts[2]['lines'][kwh+1]) * 0.39
-            return jsonify({"carbon emission" : str(carbon) + " kg CO2e"})
-
-        elif file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-            extracted_texts = extract_text_from_image(Image.open(file))
-            # Add any further processing if needed
-            kwh1 = extracted_texts.index("Total units")
-            carbon1 = float(extracted_texts[kwh1+1]) * 0.39
-            return jsonify({"carbon emission" : str(carbon1) + " kg CO2e"})
-            #return jsonify({"pages" : extracted_texts})
-
-
-        else:
-            return jsonify({'error': 'Unsupported file format.'}), 400
-            pass
-
-    elif emirate == 'Sharjah':
-        if file.filename.lower().endswith('.pdf'):
-            extracted_texts = extract_text_from_pdf(file)
-            # Add any further processing if needed
+            carbon1 = (float(extracted_texts[2]['lines'][kwh + 1]) * 0.39)/1000
+        elif emirate == 'dubai':
+            carbon = extracted_texts[1]['lines'].index("Kg CO2e")
+            carbon1 = (float(extracted_texts[1]['lines'][carbon + 1]))/1000
+        elif emirate == 'sharjah':
             kwh = extracted_texts[0]['lines'].index("Consumption")
-            carbon = float(extracted_texts[0]['lines'][kwh+1]) * 0.40
-            return jsonify({"carbon emission" : str(carbon) + " kg CO2e"})
-
-        elif file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-            extracted_texts = extract_text_from_image(Image.open(file))
-            # Add any further processing if needed
-            #return jsonify({"pages": extracted_texts})
-            kwh = extracted_texts.index("Previous Reading Date Consumption")
-            carbon = float(extracted_texts[kwh+2]) * 0.40
-            return jsonify({"carbon emission" : str(carbon) + " kg CO2e"})
-
-
-        else:
-            return jsonify({'error': 'Unsupported file format.'}), 400
-
-    elif emirate == 'Umm Al Quwain':
-        if file.filename.lower().endswith('.pdf'):
-            extracted_texts = extract_text_from_pdf(file)
-            # Add any further processing if needed
+            carbon1 = (float(extracted_texts[0]['lines'][kwh + 1]) * 0.40)/1000
+        elif emirate in ['ajman', 'fujairah', 'umm al quwain', 'ras al khaimah']:
             carbon = extracted_texts[0]['lines'].index("upto 800")
-            return jsonify({"carbon emission" : str(carbon-1) + " kg CO2e"})
-
-        elif file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-            extracted_texts = extract_text_from_image(Image.open(file))
-            carbon = extracted_texts.index("upto 800")
-            return jsonify({"carbon emission" : str(extracted_texts[carbon-1]) + " kg CO2e"})
+            carbon1 = (float(extracted_texts[0]['lines'][carbon - 1]))/1000
         else:
-            return jsonify({'error': 'Unsupported file format.'}), 400
+            return {'error': 'Unsupported emirate.'}
+
+    elif inp_file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        extracted_texts = extract_text_from_image(Image.open(inp_file))
+        # Add any further processing if needed
+
+        if emirate == 'abu dhabi':
+            kwh1 = extracted_texts.index("Total units")
+            carbon1 = (float(extracted_texts[kwh1 + 1]) * 0.39)/1000
+        elif emirate == 'dubai':
+            carbon = extracted_texts.index("Kg CO2e")
+            carbon1 = (float(extracted_texts[carbon + 1]))/1000
+        elif emirate == 'sharjah':
+            kwh = extracted_texts.index("Previous Reading Date Consumption")
+            carbon1 = (float(extracted_texts[kwh + 2]) * 0.40)/1000
+        elif emirate in ['ajman', 'fujairah', 'umm al quwain', 'ras al khaimah']:
+            carbon = extracted_texts.index("upto 800")
+            carbon1 = (float(extracted_texts[carbon - 1]))/1000
+        else:
+            return {'error': 'Unsupported emirate.'}
+
     else:
-        return "Invalid emirate selection"
-
-    return "File uploaded and processed successfully"
+        return {'error': 'Unsupported file format.'}
 
 
+    return jsonify({"total_carbon_emission": str(carbon1)})
 
 
 
+
+@app.route('/vehicle2', methods=['POST'])
+def vehicle2():
+    num_veh = int(request.form.get("vehicle_no", 1))
+
+    if num_veh!=0:
+        veh = []
+        for i in range(1, num_veh + 1):
+            veh_type = request.form.get(f"Vehicle{i}")
+            fuel = request.form.get(f"Fuel Type{i}")
+            distance = float(request.form.get(f"Distance{i}"))
+
+            emission = trans_emission(fuel, veh_type, distance)
+            veh.append(emission)
+
+        veh_tot = sum(veh)/1000
+    else:
+        veh_tot=0
+    return jsonify({"vehicle_emission": str(veh_tot)})
+
+@app.route('/flight2', methods=['POST'])
+def flight2():
+    url = "https://www.carboninterface.com/api/v1/estimates"
+    headers = {
+            "Authorization": "Bearer Eq8vmxlGJFDQJWlxXPb5A",
+            "Content-Type": "application/json"
+        }
+    passengers = request.form["passengers"]
+    num_legs = int(request.form.get("Trips", 1))  # Number of trips, defaulting to 1 if not provided
+
+    if (num_legs != 0 and passengers != 0) or num_legs != 0:
+            legs = []
+            for i in range(1, num_legs + 1):
+                departure_airport = request.form[f"From{i}"]
+                destination_airport = request.form[f"To{i}"]
+                legs.append({
+                    "departure_airport": departure_airport,
+                    "destination_airport": destination_airport
+                })
+
+            payload = {
+                "type": "flight",
+                "passengers": passengers,
+                "legs": legs,
+                "distance_unit": "km",
+            }
+
+            response = requests.post(url, headers=headers, data=json.dumps(payload))
+            carbon_trans =float(response.json()["data"]["attributes"]["carbon_mt"])
+    else:
+            carbon_trans = 0
+    return jsonify({"flight_emission": str(carbon_trans) })
 
 
 
